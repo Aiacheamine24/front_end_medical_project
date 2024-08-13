7a32b1acd806cab06b4ba735d329f54c4ebd3114
“Amine <Amine.Aiache@ultra-ft.com>
2024-08-12 23:50:29 -0400

    Update BASE_URL to use the production API endpoint

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://localhost:5000/api/v1";
+export const BASE_URL = "https://goldfish-app-7y9ue.ondigitalocean.app/api/v1";
 export const token = localStorage.getItem("token");
